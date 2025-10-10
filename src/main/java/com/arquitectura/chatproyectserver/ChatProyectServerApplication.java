894ccf9bff0271a40c10a42365708f00dc320567
nicolaslozanoc12 <nicolaslozanoc12@gmail.com>
2025-10-10 10:06:34 -0500

    Base de proyecto en spring boot

---
+++ src/main/java/com/arquitectura/chatproyectserver/ChatProyectServerApplication.java
@@ -0,0 +1,13 @@
+package com.arquitectura.chatproyectserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatProyectServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatProyectServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arquitectura/chatproyectserver/ChatProyectServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arquitectura.chatproyectserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatProyectServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
