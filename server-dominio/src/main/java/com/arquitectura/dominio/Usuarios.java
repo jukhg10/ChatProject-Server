d05b9a71d3774d30557b798fe5f516a40e0c658d
nicolaslozanoc12 <nicolaslozanoc12@gmail.com>
2025-10-11 20:02:04 -0500

    se creo la configuracion de la persistencia y se creo una clase con una insercion de datos, tambien se agrego un dockerCompose con mysql

--- server-dominio/src/main/java/com/arquitectura/dominio/Usuarios.java
+++
@@ -1,4 +0,0 @@
-package com.arquitectura.dominio;
-
-public class Usuarios {
-}
--- server-persistencia/src/main/java/com/arquitectura/configdb/ConfiguracionPersistencia.java
+++ server-persistencia/src/main/java/com/arquitectura/configdb/ConfiguracionPersistencia.java
@@ -6,27 +6,34 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
-@PropertySource("file:./config/persistencia.properties")
-@EnableJpaRepositories(basePackages = "com.arquitectura.configdb")
+@PropertySource("file:./config/database.properties")
+@EnableJpaRepositories(basePackages = "com.arquitectura.persistence")
 public class ConfiguracionPersistencia {
-    // Spring leerá el valor de 'db.url' del archivo y lo inyectará en esta variable
+    // Spring leerá el archivo y lo inyectará a estas variables
     @Value("${db.url}")
     private String dbUrl;
-
     @Value("${db.username}")
     private String dbUsername;
-
     @Value("${db.password}")
     private String dbPassword;
-
     @Value("${db.driver}")
     private String dbDriver;
+    // --- Propiedades de Hibernate (las leeremos del mismo archivo) ---
+    @Value("${hibernate.dialect}")
+    private String hibernateDialect;
+    @Value("${hibernate.hbm2ddl.auto}")
+    private String hibernateHbm2ddlAuto;
+    @Value("${hibernate.show_sql}")
+    private String hibernateShowSql;
 
     @Bean
     public DataSource dataSource() {
@@ -36,7 +43,6 @@ public class ConfiguracionPersistencia {
         dataSource.setUsername(dbUsername);
         dataSource.setPassword(dbPassword);
 
-
         return dataSource;
     }
 
@@ -44,9 +50,24 @@ public class ConfiguracionPersistencia {
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
         LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
         emf.setDataSource(dataSource()); // Usa el DataSource
-        emf.setPackagesToScan("com.arquitectura."); // Dónde buscar tus clases @Entity
+        emf.setPackagesToScan("com.arquitectura.domain"); // Dónde buscar tus clases @Entity
         emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter()); // Le decimos que usaremos Hibernate
         emf.setJpaProperties(hibernateProperties()); // Le pasamos las propiedades de Hibernate
         return emf;
+    }
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return transactionManager;
+    }
+
+    private java.util.Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", hibernateHbm2ddlAuto);
+        properties.setProperty("hibernate.dialect", hibernateDialect);
+        properties.setProperty("hibernate.show_sql", hibernateShowSql);
+        properties.setProperty("hibernate.format_sql", "true"); // Opcional pero recomendado
+        return properties;
     }
 }
